2158bbdafd5c0577c2afb2c1f1c558313efa7fd2
raximvodil <raximvodilrozbek@gmail.com>
2022-07-25 13:28:37 +0500

    feat: success!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let arr = String(n).replace("-", "").split("").reverse();
+
+    return Number(arr.join(""));
+};
